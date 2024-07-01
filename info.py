1989a267f2a47037de1ee252888df5c6b7ed47cc
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-07-01 12:02:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = 26042863
 API_HASH     = "d4fabc00b0345cd3f0ccdc0c9b750f6e"
-BOT_TOKEN    = ""
+BOT_TOKEN    = "6934545157:AAEdEtjjzTslvgPrDXEMT1AVpQrvDiscfsc"
 SESSION      = "BQD6zg0AR7pr3eKYaFz5meqeAIFbsbaAQSBYAFSxtv1lHBbYBBNyAVBDEciuHJHWKy5uYgjykqw4SOryaNYSWzQ_1OFBsRRPPYGz8wxScabcXAfIm8vJ6kgXkXwDs87n8YP2iZFRphKkHQfVPdCOAY"
 DATABASE_URI = "mongodb+srv://SGBACKUP4:SGBACKUP@cluster0.2o4audp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = "-1001994332079"
 ADMIN        = 919169586
-CHANNEL      = "SGBACKUP"
+CHANNEL      = ""
