4a9315ed867b9346fb3094983e6a17119541bfa8
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-07-01 11:23:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  =
-ADMIN        = 1255023013
-CHANNEL      = "RahulReviews"
+API_ID       = 26042863
+API_HASH     = "d4fabc00b0345cd3f0ccdc0c9b750f6e"
+BOT_TOKEN    = "6934545157:AAGHlmQX9GcfFj02Msm72KFli9gmXODSE6w"
+SESSION      = "BQD6zg0AR7pr3eKYaFz5meqeAIFbsbaAQSBYAFSxtv1lHBbYBBNyAVBDEciuHJHWKy5uYgjykqw4SOryaNYSWzQ_1OFBsRRPPYGz8wxScabcXAfIm8vJ6kgXkXwDs87n8YP2iZFRphKkHQfVPdCOAY"
+DATABASE_URI = "mongodb+srv://SGBACKUP4:SGBACKUP@cluster0.2o4audp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = "-1001994332079"
+ADMIN        = 919169586
+CHANNEL      = "SGBACKUP"
